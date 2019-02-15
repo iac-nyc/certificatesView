1a1b21a17bdc87960c0d7dd9403138a8a3fb861c
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-02-15 13:53:22 -0500

    Initial Setup

---
+++ certificates.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#photos li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  });
+    
+  $("#myInput").keydown(function(){
+    $("#myInput").css("background-color", "#7B68EE");
+    $("#myInput").css("color", "#fff");  
+  });
+  $("#myInput").keyup(function(){
+    $("#myInput").css("background-color", "	#48D1CC");
+    $("#myInput").css("color", "#fff");  
+  });    
+});
+
