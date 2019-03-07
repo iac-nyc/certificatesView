4280754c7fee60cd63fbfeb7b60cd36d2cc94bc4
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-03-07 09:41:16 -0500

    nodeJS server side ceritificate added

--- certificates.js
+++ certificates.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $("#myInput").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-         $(".photos li").filter(function() {  
-         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    var value = $(this).val().toLowerCase();
+    $(".photos li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
       });
   });
-    
+
   $("#myInput").keydown(function(){
     $("#myInput").css("background-color", "#7B68EE");
     $("#myInput").css("color", "#fff");  
